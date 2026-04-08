6aa39233c1c48d4f5b273b81b20991b5b469288f
Javi Mejias Reverté <mejiasrjavi@gmail.com>
2026-04-08 21:22:29 +0200

    First version of the terraform script
    - It causes an error due to the t3.micro lack in the selected availability zone.

--- Development/Experiments/Final-version-V0.0.md
+++ Development/Experiments/Final-version-V0.0.md
@@ -3,19 +3,28 @@ final-version: V0.0
 environment: AWS
 ---
 # Description
+## Implications of migrating from Docker containers in localhost to AWS
 
-> [!warning] Important
-> Some changes have been done in respect to the most final-version-like experiment ([[Development/Experiments/RedisShardingReplicationSentinel(3+3*2+3)+Gateway|RedisShardingReplicationSentinel(3+3*2+3)+Gateway]]). Such involve reducing the number of instances used by the entire system to 9. This is because this task will be uploaded to AWS, and that is the exact number of instances we are limited to create.
-> 
-> To reduce the amount of instances we had been to:
-> - Reduce the number of shards from 3 to 2, as can be seen at [[#Components]].
-> - Move the Redis Sentinels (3 instances) to live inside the *load balancer* and *gateways*. This is indeed recommended in the following [Redis article](https://redis.io/docs/latest/operate/oss_and_stack/management/sentinel/#:~:text=The%20three%20Sentinel%20instances%20should%20be%20placed%20into%20computers%20or%20virtual%20machines%20that%20are%20believed%20to%20fail%20in%20an%20independent%20way.%20So%20for%20example%20different%20physical%20servers%20or%20Virtual%20Machines%20executed%20on%20different%20availability%20zones.).
+### Instances number limit
+Some changes have been done in respect to the most final-version-like experiment ([[Development/Experiments/RedisShardingReplicationSentinel(3+3*2+3)+Gateway|RedisShardingReplicationSentinel(3+3*2+3)+Gateway]]). Such involve reducing the number of instances used by the entire system to 9. This is because this task will be uploaded to AWS, and that is the exact number of instances we are limited to create.
+ 
+To reduce the amount of instances we had been to:
+- Reduce the number of shards from 3 to 2, as can be seen at [[#Components].
+- Move the Redis Sentinels (3 instances) to live inside the *load balancer* and *gateways*. This is indeed recommended in the following [Redis article](https://redis.io/docs/latest/operate/oss_and_stack/management/sentinel/#:~:text=The%20three%20Sentinel%20instances%20should%20be%20placed%20into%20computers%20or%20virtual%20machines%20that%20are%20believed%20to%20fail%20in%20an%20independent%20way.%20So%20for%20example%20different%20physical%20servers%20or%20Virtual%20Machines%20executed%20on%20different%20availability%20zones.).
+
+### Docker DNS
+Throughout the development, we had been using Docker's DNS to resolve the hardcoded names of the requests' targets. We no longer can rely on this since we won't be keeping all the containers in a Docker network. One possible solution to this is to use the bare IPs.
+
+#further-work Evidently, this should be changed to make the system truly scalable.
 ## Components
 
 This final version has the following instances:
 - 2 Redis shards (that are also sentinels) = (1 master + 2 replicas) per shard * 2 shards = 2 masters + 4 replicas = 6 instances
 - 1 load balancer
 - 2 gateways in the cluster
+
+# Deploying
+
 
 # Variants
 
---
+++ final-versions/V0.0/aws/main.tf
@@ -0,0 +1,153 @@
+# 1. PROVIDER CONFIGURATION
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 2. NETWORK INFRASTRUCTURE
+resource "aws_vpc" "redis_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = { Name = "redis-cluster-vpc" }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.redis_vpc.id
+  tags = { Name = "redis-cluster-igw" }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.redis_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true 
+  tags = { Name = "redis-cluster-subnet" }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.redis_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 3. SECURITY GROUP (FIREWALL)
+resource "aws_security_group" "redis_sg" {
+  name        = "redis_cluster_sg"
+  description = "Allow SSH, HTTP, and internal cluster traffic"
+  vpc_id      = aws_vpc.redis_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 4. DYNAMIC AMI DATA FETCH
+# This queries AWS for the latest official Ubuntu 24.04 AMI in us-east-1
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Official Canonical AWS Account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+variable "key_name" {
+  default = "SD-task1-key-pair"
+}
+
+# 5. EC2 INSTANCES
+resource "aws_instance" "control_plane" {
+  count                  = 3
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.redis_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "control-plane-node-${count.index + 1}"
+    Role = "edge-and-sentinel"
+  }
+}
+
+resource "aws_instance" "data_plane_masters" {
+  count                  = 2
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.redis_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "redis-master-${count.index == 0 ? "a" : "b"}"
+    Role = "database-master"
+  }
+}
+
+resource "aws_instance" "data_plane_replicas" {
+  count                  = 4
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.redis_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "redis-replica-${count.index < 2 ? "a" : "b"}${count.index % 2 + 1}"
+    Role = "database-replica"
+  }
+}
+
+# 6. OUTPUTS
+output "control_plane_private_ips" {
+  value = aws_instance.control_plane[*].private_ip
+}
+
+output "control_plane_public_ips" {
+  value = aws_instance.control_plane[*].public_ip
+}
+
+output "master_private_ips" {
+  value = aws_instance.data_plane_masters[*].private_ip
+}
+
+output "replica_private_ips" {
+  value = aws_instance.data_plane_replicas[*].private_ip
+}
