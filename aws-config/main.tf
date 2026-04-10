9a62baae80961e8fc82ff9d501dc1c4fe2f357ab
ivanmoliinero <imolimort@gmail.com>
2026-04-10 22:41:02 +0200

    Eliminated ingress rule for HTTP connections that was used in previous versions for NGINX checking of node availability

--- aws-config/main.tf
+++ aws-config/main.tf
@@ -120,15 +120,6 @@ resource "aws_security_group" "custom_sg" {
     cidr_blocks = [aws_vpc.custom_vpc.cidr_block]
   }
 
-  # HTTP internal comms to obtain state of RabbitMQ servers
-  ingress {
-    description = "Allow HTTP traffic on port 80 for Nginx cloud-init check"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.custom_vpc.cidr_block]
-  }
-
   # REDIS internal comms
   ingress {
     description = "Redis service"
